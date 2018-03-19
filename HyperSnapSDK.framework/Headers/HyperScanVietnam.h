9c94af6ea78e20063205965e5405a7fe315f54b1
Srinija <srinija@hyperverge.co>
2018-03-19 21:04:43 +0530

    Added Framework

---
+++ HyperSnapSDK.framework/Headers/HyperScanVietnam.h
@@ -0,0 +1,19 @@
+//
+//  HyperSnapSDK.h
+//  HyperSnapSDK
+//
+//  Created by Srinija on 05/03/18.
+//  Copyright © 2018 hyperverge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HyperSnapSDK.
+FOUNDATION_EXPORT double HyperSnapSDKVersionNumber;
+
+//! Project version string for HyperSnapSDK.
+FOUNDATION_EXPORT const unsigned char HyperSnapSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HyperSnapSDK/PublicHeader.h>
+
+
